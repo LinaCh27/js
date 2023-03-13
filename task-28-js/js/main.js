d7e7a75369ded6b411221274208ced70ed37f5a1
LinaCh27 <125282932+LinaCh27@users.noreply.github.com>
2023-03-13 15:48:35 +0100

    made if/else statement

---
+++ task-28-js/js/main.js
@@ -0,0 +1,8 @@
+let number = 12;
+if (number > 10) {
+    console.log('The given number is greater than 10');
+}
+
+else if (number < 10) {
+    console.log('The given number is not greater than 10');
+}
