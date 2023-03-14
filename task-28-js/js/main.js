9af5db7be12fcb67ed4511e85fddeac912224289
LinaCh27 <125282932+LinaCh27@users.noreply.github.com>
2023-03-14 16:50:19 +0100

    finished task

--- task-28-js/js/main.js
+++ task-28-js/js/main.js
@@ -1,8 +1,18 @@
 let number = 12;
 if (number > 10) {
     console.log('The given number is greater than 10');
-}
-
-else if (number < 10) {
+} else if (number < 10) {
     console.log('The given number is not greater than 10');
 }
+
+
+// function coffeeExpress(water, coffee, milk, sugar) {
+//     return 'coffee is made of: ' + water + ' ' + coffee + ' ' + milk + ' ' + sugar;
+// }
+
+// const cup = coffeeExpress('filtered water', 'arabica', 'soya', 'brown');
+// console.log(cup);
+
+// const cup2 = coffeeExpress('boiled water', 'arabica', 'cow milk', 'white');
+// console.log(cup2);
+
---
+++ task-29-js/js/main.js
@@ -0,0 +1,8 @@
+function multiply(num1, num2) {
+    let result = num1 * num2;
+
+    return result;
+}
+
+console.log(multiply(7, 3));
+
