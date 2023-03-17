f197cdbec5cace52a01e72a6cd9b824ef98153ff
LinaCh27 <125282932+LinaCh27@users.noreply.github.com>
2023-03-17 17:17:54 +0100

    created function with number range

---
+++ task-35-js/js/main.js
@@ -0,0 +1,13 @@
+function numRange(n) {
+    if (n >= 100 && n <= 200) {
+        console.log("The number is in the range.");
+    } else {
+        console.log("The number is not in range.");
+    }
+    
+}
+
+numRange(127);
+numRange(93);
+
+
